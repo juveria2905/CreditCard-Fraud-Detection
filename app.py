6c063222e09fe3e504dea864b2b05409ff9a1ffa
Sk Juveria Tabassum <154406153+juveria2905@users.noreply.github.com>
2026-04-29 14:39:57 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request
+import numpy as np
+import joblib
+
+app = Flask(__name__)
+
+model = joblib.load("fraud_model.pkl")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        features = []
+
+        features.append(float(request.form["Time"]))
+        for i in range(1, 29):
+            features.append(float(request.form[f"V{i}"]))
+        features.append(float(request.form["Amount"]))
+
+        features = np.array(features).reshape(1, -1)
+
+        prediction = model.predict(features)[0]
+        probability = model.predict_proba(features)[0][1]
+
+        if prediction == 1:
+            result = "⚠ Fraud Detected"
+            color = "danger"
+        else:
+            result = "✅ Safe Transaction"
+            color = "success"
+
+        return render_template(
+            "index.html",
+            prediction_text=result,
+            probability=round(probability * 100, 2),
+            alert_color=color
+        )
+
+    except:
+        return render_template("index.html", prediction_text="Error in input")
+
+if __name__ == "__main__":
+    app.run(debug=True)
